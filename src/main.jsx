4c4cac0e54ddf4f6a2a24b81f042be512fc80af8
Mpho Itumeleng <mphoitumeleng021@gmail.com>
2025-03-19 14:39:42 +0200

    updated the main.jsx file to handle queries

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import the necessary QueryClientProvider
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create a QueryClient instance
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}> {/* Wrap your app with QueryClientProvider */}
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
+);
