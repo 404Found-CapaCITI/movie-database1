25744102b49595dadb11a994d5a1a648f2f19303
Mpho Itumeleng <mphoitumeleng021@gmail.com>
2025-03-19 14:27:05 +0200

    created movie list component to display content

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ movieList,}) => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-8">
+      {movieList.map((movie) => (
+        <Link to={`/movie/${movie.id}`} key={movie.id}>
+          <MovieCard
+            id={movie.id}
+            name={movie.original_title}
+            posterPath={movie.poster_path}
+            releaseDate={movie.release_date}
+            vote={movie.vote_average.toFixed(1)}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
