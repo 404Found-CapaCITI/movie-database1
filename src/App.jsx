3c07c87e45fae5887d1b1e8a1b302d03b3a156a8
isamonyeseala <221768963@edu.vut.ac.za>
2025-03-20 10:19:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ import MovieDetails from "./pages/MovieDetails";
 const App = () => {
   return (
     <>
-      {/* ✅ Navigation Bar */}
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-              Movie Database
-            </Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
       {/* ✅ Page Layout Wrapper */}
       <Container>
         <Routes>
