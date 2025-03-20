e2b2cff7c153d645f31249c27a2b04aba9d89f0d
isamonyeseala <221768963@edu.vut.ac.za>
2025-03-20 10:09:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/HomePage.jsx";
-import MovieDetails from "../pages/MovieDetails.jsx"; // ✅ Uncommented to use in routing
+import Home from "./pages/HomePage.jsx";
+// import MovieDetails from "./pages/MovieDetails.jsx";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />{" "}
-      {/* ✅ Fixed routing */}
+      {/* <Route path="/movie/:id" element={<MovieDetails />} /> */}
     </Routes>
   );
 };
