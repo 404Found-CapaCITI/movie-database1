330870c62a3b05f8805d48f60de0e14f3dfe8e5e
isamonyeseala <221768963@edu.vut.ac.za>
2025-03-20 10:10:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/HomePage.jsx";
+import Home from "./pages/HomePage";
 // import MovieDetails from "./pages/MovieDetails.jsx";
 
 const App = () => {
