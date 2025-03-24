ca7b3fffd1c45170f79f4ee6aa2a3121afed5dca
isamonyeseala <221768963@edu.vut.ac.za>
2025-03-24 16:11:08 +0200

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-const SearchBar = ({ searchTerm, setSearchTerm }) => {
-  const handleInputChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
+const SearchBar = ({ searchTerm = "", setSearchTerm }) => {
   return (
     <input
       type="text"
       placeholder="Search for a movie..."
-      value={searchTerm}
-      onChange={handleInputChange}
+      value={searchTerm || ""}
+      onChange={(e) => setSearchTerm(e.target.value || "")}
       className="p-2 border rounded w-full"
+      aria-label="Search Movies"
     />
   );
 };
