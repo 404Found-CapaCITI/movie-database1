c507c18736c9f37594fbda7c3632a43fca0cb183
isamonyeseala <221768963@edu.vut.ac.za>
2025-03-24 12:24:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage"; // Ensure the import matches the filename
+import HomePage from "./pages/HomePage";
 import MovieDetails from "./pages/MovieDetails";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/movie/:id" element={<MovieDetails />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
